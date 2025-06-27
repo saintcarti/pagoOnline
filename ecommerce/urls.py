964b0f3cdc781b08dae1ca17bcb351306e6cc435
saintcarti <xa.liberona@duocuc.cl>
2025-06-27 18:43:37 -0400

    cambios generales

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import index,login,register
+from .views import index,login,register,products
 
 
 urlpatterns = [
     path('',index,name='index-page'),
     path('login/',login,name='login-page'),
-    path('register/',register,name='register-page')
+    path('register/',register,name='register-page'),
+    path('products/',products,name='products-page')
 ]
