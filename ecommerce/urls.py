f9eb245dfeb979b3ac851a57624258233bb43f24
MauricioGHUB <Mau.gatica@duocuc.cl>
2025-06-27 19:55:46 -0400

    list-product_view

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import index,login,register,products
+from .views import index,login,register,products,list_products
 
 
 urlpatterns = [
     path('',index,name='index-page'),
     path('login/',login,name='login-page'),
     path('register/',register,name='register-page'),
-    path('products/',products,name='products-page')
+    path('products/',products,name='products-page'),
+    path('list-products/', list_products, name='list-products'),
 ]
--- ecommerce/views.py
+++ ecommerce/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from miPaypal.models import Product
-
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
@@ -20,3 +20,10 @@ def products(request):
     product = Product.objects.all()
 
     return render(request,'ecommerce/products.html',{"products":product})
+
+@login_required
+def list_products(request):
+    products = Product.objects.all()
+    return render(request, 'dashboard-panel/crud-products/list-product.html', {
+        "products": products
+    })
