fe5e64db222719da2fa4d0d3d1e488615a84c5ae
saintcarti <xa.liberona@duocuc.cl>
2025-06-28 01:40:06 -0400

    sda

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -2,26 +2,11 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-<<<<<<< HEAD
-    ##ecommerce urls
-    path('',index,name='index-page'),
-    path('products/',products,name='products-page'),
-    path('about/',about,name='about-page'),
-    path('contact/',contact_view,name='contact-page'),
-    ##Auth urls
-    path('login/',login,name='login-page'),
-    path('register/',register,name='register-page'),
-    ##Panel administracion urls
-    path('dashboard-panel/',dashboard,name='dashboard-panel-page'),
-    path('admin-settings/',admin_settings,name='admin-settings-page'),
-    path('admin-profile/',admin_profile,name='admin-profile-page'),
-    ##CRUD Products
-=======
     # ecommerce urls
     path('', index, name='index-page'),
     path('products/', products, name='products-page'),
     path('about/', about, name='about-page'),
-    path('contact/', contact, name='contact-page'),
+    path('contact/', contact_view, name='contact-page'),
     
     # Auth urls
     path('login/', login, name='login-page'),
@@ -34,7 +19,6 @@ urlpatterns = [
     path('admin-profile/', admin_profile, name='admin-profile-page'),
     
     # CRUD Products
->>>>>>> 23b77a92fca07c3feeafc218d49db41e5c9da1a1
     path('list-products/', list_products, name='list-products'),
     path('create-product/', crear_pview, name='create-product'),
     path('update-product/<int:pk>/', update_product, name='update-product'),
